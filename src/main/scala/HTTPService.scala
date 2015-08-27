6598ea34aaf6764a1894a9cc25e6bb1a3961eca7
James Duncan Davidson <duncandavidson@me.com>
2015-08-27 16:24:48 +0200

    Clean up imports

--- src/main/scala/HTTPService.scala
+++ src/main/scala/HTTPService.scala
@@ -1,7 +1,6 @@
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, Materializer}
+import akka.stream.ActorMaterializer
 import akka.http.scaladsl.Http
-// import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model.HttpMethods._
 import akka.http.scaladsl.model._
 
